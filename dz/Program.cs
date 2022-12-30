82deee04f3909320d937daf6af6a0470df398f21
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-30 20:44:39 +0700

    66

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,5 +1,36 @@
-﻿
-Console.WriteLine("Введите число ");
-int a = Convert.ToInt32(Console.ReadLine());
-a = (a / 10) % 10;
-Console.WriteLine(a);
+public static class Lesson457
+{
+    static void Main()
+    {
+   
+// Ввод чисел M и N
+
+ Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SUM(m, n);
+
+// Расчёт суммы чисел от M до N
+
+int SumMN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumMN(m, n);
+        return result;
+    }
+}
+// Метод вывода чисел от M до N
+      
+void SUM(int m, int n)
+    {
+        Console.Write (SumMN(m - 1, n));
+    }
+
+}}
